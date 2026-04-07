8d909c9d9896643475247539e4ca2fe44a56c0b8
Anisha Kc <anishakc90@gmail.com>
2026-04-07 20:12:47 +0545

    Add API documentation using apidoc

--- api/routes/appointments.js
+++ api/routes/appointments.js
@@ -4,6 +4,8 @@ const db = require("../database");
 
 /**
  * @api {get} /appointments Get all appointments
+ * @apiName GetAppointments
+ * @apiGroup Appointments
  */
 router.get("/", (req, res) => {
   db.all("SELECT * FROM appointments", [], (err, rows) => {
@@ -12,7 +14,30 @@ router.get("/", (req, res) => {
 });
 
 /**
+ * @api {get} /appointments/:id Get single appointment
+ * @apiName GetAppointmentById
+ * @apiGroup Appointments
+ * @apiParam {Number} id Appointment unique ID
+ */
+router.get("/:id", (req, res) => {
+  db.get(
+    "SELECT * FROM appointments WHERE id = ?",
+    [req.params.id],
+    (err, row) => {
+      res.json(row);
+    }
+  );
+});
+
+/**
  * @api {post} /appointments Create appointment
+ * @apiName CreateAppointment
+ * @apiGroup Appointments
+ * @apiBody {String} first_name First name of patient
+ * @apiBody {String} last_name Last name of patient
+ * @apiBody {String} date Appointment date
+ * @apiBody {String} time Appointment time
+ * @apiBody {String} treatment Treatment type
  */
 router.post("/", (req, res) => {
   const { first_name, last_name, date, time, treatment } = req.body;
@@ -29,12 +54,22 @@ router.post("/", (req, res) => {
 
 /**
  * @api {put} /appointments/:id Update appointment
+ * @apiName UpdateAppointment
+ * @apiGroup Appointments
+ * @apiParam {Number} id Appointment unique ID
+ * @apiBody {String} first_name First name of patient
+ * @apiBody {String} last_name Last name of patient
+ * @apiBody {String} date Appointment date
+ * @apiBody {String} time Appointment time
+ * @apiBody {String} treatment Treatment type
  */
 router.put("/:id", (req, res) => {
   const { first_name, last_name, date, time, treatment } = req.body;
 
   db.run(
-    `UPDATE appointments SET first_name=?, last_name=?, date=?, time=?, treatment=? WHERE id=?`,
+    `UPDATE appointments
+     SET first_name=?, last_name=?, date=?, time=?, treatment=?
+     WHERE id=?`,
     [first_name, last_name, date, time, treatment, req.params.id],
     () => res.json({ message: "Updated" })
   );
@@ -42,6 +77,9 @@ router.put("/:id", (req, res) => {
 
 /**
  * @api {delete} /appointments/:id Delete appointment
+ * @apiName DeleteAppointment
+ * @apiGroup Appointments
+ * @apiParam {Number} id Appointment unique ID
  */
 router.delete("/:id", (req, res) => {
   db.run("DELETE FROM appointments WHERE id=?", [req.params.id], () => {
